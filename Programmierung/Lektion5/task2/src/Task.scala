cf753147b02cbf58d130506f27a000a95f12bf47
x21L <lukas.wais@outlook.de>
2023-01-22 22:46:52 +0100

    Added description and exercises for Lektion5.task2.iml and Lektion5.task3.iml

--- Programmierung/Lektion5/task2/src/Task.scala
+++ Programmierung/Lektion5/task2/src/Task.scala
@@ -1,9 +1,9 @@
 class Task {
   def convertNumberToString(x: Int): String = {
     x match
-    case 0 => "zero"
-    case 1 => "one"
-    case 2 => "two"
-    case _´=> "some other number"
+      case 0 => "zero"
+      case 1 => "one"
+      case 2 => "two"
+      case _ => "some other number"
   }
 }
